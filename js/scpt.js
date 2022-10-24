d7ef57a5fbb0deb1e91ed5db2073f1068d8dd2c1
fbikrg <1820105@aluno.univesp.br>
2022-10-24 11:27:41 -0300

    24102022

---
+++ js/scpt.js
@@ -0,0 +1,45 @@
+//const para = document.querySelector("p")
+//fetch("http://127.0.0.1:8000/plants/1/?format=json")
+//.then(Response => {
+//    return Response.json()
+//})
+//.then(data1 => {
+//    var advise = data1.plantName
+//    para.innerText = advise
+//})
+
+
+const url = "http://127.0.0.1:8000/plants/"
+const loadingElement = document.querySelector("#loading")
+const postsContainer = document.querySelector("#posts-container")
+
+// Get all posts
+async function getAllPosts() {
+    const response = await fetch(url);
+        //console.log(response);   
+
+    const data = await response.json();
+        //console.log(data);
+        //loadingElement.classList.add("hide");
+    
+    data.map((post)=> {
+        const div = document.createElement("div")
+        const plantName = document.createElement("h2")
+        const soilMoisture = document.createElement("p")
+        const airHumidity = document.createElement("a")
+
+        plantName.innerText = post.plantName;
+        soilMoisture.innerText = post.soilMoisture;
+        airHumidity.innerText = post.airHumidity
+        //link.setAttribute("href", `/post.html?id=${post.id}`)
+
+        div.appendChild(plantName);
+        div.appendChild(soilMoisture);
+        div.appendChild(airHumidity);
+
+        postsContainer.appendChild(div);
+
+    })
+}
+
+getAllPosts();
