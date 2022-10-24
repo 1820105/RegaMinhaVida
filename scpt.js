8955aeca92de6d3fa4bb621a9100764d7c5cea3a
fbikrg <1820105@aluno.univesp.br>
2022-10-24 08:40:03 -0300

    24102022

--- scpt.js
+++ scpt.js
@@ -1,7 +1,9 @@
-const URL = '';
-fetch("http://127.0.0.1:8000/plants/?format=json")
-.then((res) => res.json())
-.then((data) => {
-    console.log(data);
-    document.querySelector('#plantName').innerHTML = data.name
+const para = document.querySelector("p")
+fetch("http://127.0.0.1:8000/plants/1/?format=json")
+.then(Response => {
+    return Response.json()
+})
+.then(data1 => {
+    var advise = data1.plantName
+    para.innerText = advise
 })
